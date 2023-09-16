cb3d2e6ee96f71f7c541f05319e200442bcf882a
Reeve Fernandes <ferna312@msu.edu>
2023-09-16 17:27:16 -0400

    Login Compeleted

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // @mui
-import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox } from '@mui/material';
+import { Stack, IconButton, InputAdornment, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // components
 import Iconify from '../../../components/iconify';
@@ -10,19 +10,54 @@ import Iconify from '../../../components/iconify';
 
 export default function LoginForm() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [showPassword, setShowPassword] = useState();
+  const [role, setRole] = useState();
 
-  const [showPassword, setShowPassword] = useState(false);
+  const onChangeHandler = (e) => {
+    if (e.target.name === 'email') {
+      setEmail(e.target.value);
+    }
+    if (e.target.name === 'password') {
+      setPassword(e.target.value);
+    }
+    if (e.target.name === 'role') {
+      setRole(e.target.value);
+    }
+  };
 
-  const handleClick = () => {
-    navigate('/dashboard', { replace: true });
+  const handleClick = async () => {
+    try {
+      const body = { email, password, role };
+      const response = await fetch(`${process.env.API_URL}/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+
+      const Res = await response.json();
+
+      if (Res) {
+        navigate('/dashboard', { replace: true });
+      } else {
+        console.log('====================================');
+        console.log('Wrong Credential');
+        console.log('====================================');
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
   return (
     <>
       <Stack spacing={3}>
-        <TextField name="email" label="Email address" />
+        <TextField name="email" label="Email address" value={email} onChange={onChangeHandler} />
 
         <TextField
+          value={password}
+          onChange={onChangeHandler}
           name="password"
           label="Password"
           type={showPassword ? 'text' : 'password'}
@@ -36,13 +71,12 @@ export default function LoginForm() {
             ),
           }}
         />
+
+        <TextField value={role} onChange={onChangeHandler} name="role" label="Role" />
       </Stack>
 
       <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-        <Checkbox name="remember" label="Remember me" />
-        <Link variant="subtitle2" underline="hover">
-          Forgot password?
-        </Link>
+        Forgot Password
       </Stack>
 
       <LoadingButton fullWidth size="large" type="submit" variant="contained" onClick={handleClick}>
